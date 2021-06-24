759fa40f71193ef1748e6bbed0c7d8ddaa4457db
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-06-24 19:54:20 -0400

    updated pop-up event handler

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 let images = document.querySelectorAll(".project-image");
 
-
 let popup = document.querySelector("#popup");
 let popupImage = document.querySelector("#popup-img");
 
 images.forEach((image) => {
-    image.addEventListener("click", (e) => {
-        console.log(e.target)
-        popupImage.src = e.target.src;
-        popup.style.display = "block";
-    })
-})
-
-
-
-popupImage.addEventListener("click", (e) => {
-    console.log(e.target)
-    e.stopPropagation();
+  image.addEventListener("click", (e) => {
+    console.log(e.target);
+    popupImage.src = e.target.src;
+    popup.style.display = "block";
+  });
 });
 
-popup.addEventListener("click", () => {
-    popup.style.display = "none";
+popup.addEventListener("click", (event) => {
+  if (event.target === popupImage) {
+    return;
+  }
+  popup.style.display = "none";
 });
