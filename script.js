4b43c4397a68e94355fda51c0ce6e61665c02bd2
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-06-24 17:23:11 -0400

    updated css and javascript

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ let images = document.querySelectorAll(".project-image");
 
 
 let popup = document.querySelector("#popup");
-let closeBtn = document.querySelector(".popup-close");
 let popupImage = document.querySelector("#popup-img");
 
 images.forEach((image) => {
     image.addEventListener("click", (e) => {
+        console.log(e.target)
         popupImage.src = e.target.src;
         popup.style.display = "block";
     })
 })
 
+
+
 popupImage.addEventListener("click", (e) => {
+    console.log(e.target)
     e.stopPropagation();
 });
 
