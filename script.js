44ee427437e3c6019b869212bfe484fdadd9e74b
Devesh Kumar <portretrieve@yahoo.com>
2024-03-07 15:18:22 -0700

    updated hamburger

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let images = document.querySelectorAll(".project-image");
+const images = document.querySelectorAll(".project-image");
+const popup = document.querySelector("#popup");
+const popupImage = document.querySelector("#popup-img");
 
-let popup = document.querySelector("#popup");
-let popupImage = document.querySelector("#popup-img");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 images.forEach((image) => {
-    image.addEventListener("click", (e) => {
-        popupImage.src = e.target.src;
-        popup.style.display = "block";
-    });
+  image.addEventListener("click", (e) => {
+    popupImage.src = e.target.src;
+    popup.style.display = "block";
+  });
 });
 
 popup.addEventListener("click", (event) => {
-    if (event.target === popupImage) {
-        return;
-    }
-    popup.style.display = "none";
+  if (event.target === popupImage) {
+    return;
+  }
+  popup.style.display = "none";
 });
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
